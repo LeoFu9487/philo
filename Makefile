814da1ce2211b47b754570892464090a81d1ee5f
LeoFu9487 <leofu890806@gmail.com>
2021-07-29 16:29:00 +0200

    Makefile

---
+++ Makefile
@@ -0,0 +1,43 @@
+NAME = philo
+
+CFLAGS = -Wall -Wextra -Werror -pthread -fsanitize=address -g
+
+CC = gcc
+
+SRCS_FILE = main.c
+
+SRCS_PATH = srcs/
+
+SRCS = $(addprefix $(SRCS_PATH), $(SRCS_FILE))
+
+OBJS = $(SRCS:.c=.o)
+
+#PUT HEADERS HERE
+HEADER_FILE = philo.h
+
+HEADER_PATH = includes/
+
+HEADER = $(addprefix $(HEADER_PATH), $(HEADER_FILE))
+
+all : $(NAME)
+	echo "Compile OK"
+
+$(NAME) : $(OBJS)
+	$(CC) $(CFLAGS) -I $(HEADER_PATH)  $(OBJS) -o $(NAME)
+
+%.o : %.c $(HEADER)
+	$(CC) $(CFLAGS) -I $(HEADER_PATH) -c $< -o $@
+
+clean :
+	rm -rf $(OBJS)
+	echo "clean OK"
+
+fclean :
+	rm -rf $(OBJS) $(NAME)
+	echo "fclean OK"
+
+re : fclean all
+
+.PHONY: all clean fclean re
+
+#.SILENT :
---
+++ includes/philo.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/09 20:55:56 by yfu               #+#    #+#             */
+/*   Updated: 2021/06/09 21:01:15 by yfu              ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+/*
+** allowed functions :
+** memset
+** printf
+** malloc
+** free
+** write
+** usleep
+** gettimeofday
+** pthread_create
+** pthread_detach
+** pthread_join
+** pthread_mutex_init
+** pthread_mutex_destroy
+** pthread_mutex_lock
+** pthread_mutex_unlock
+*/
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
