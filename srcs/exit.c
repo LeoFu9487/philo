a44e2c9b9c9a02bab0356f1c5d5a53c312edcbb1
Yu-peng Fu <yfu@z3r7p1.42lyon.fr>
2021-07-30 13:39:24 +0200

    todo : routine

--- srcs/exit.c
+++ srcs/exit.c
@@ -1,45 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yfu <yfu@student.42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/09 20:55:31 by yfu               #+#    #+#             */
-/*   Updated: 2021/07/30 13:27:33 by yfu              ###   ########lyon.fr   */
+/*   Created: 2021/07/30 13:26:23 by yfu               #+#    #+#             */
+/*   Updated: 2021/07/30 13:36:23 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-/*
-** IF error return -1, else return 0
-*/
-int	create_thread(void)
+void	exit_free(void)
 {
-	int	i;
+	int i;
 
 	i = -1;
 	while (++i < g_data.number_of_philosophers)
-		if (pthread_create(&g_data.philo[i].thread_id, NULL, routine, &g_data.philo[i]))
-		{
-			ft_putstr_fd("Error: pthread_createn\n", 2);
-			return (-1);
-		}
-	return (0);
-}
-
-int	main(int ac, char **av)
-{
-	if (parse(ac, av))
 	{
-		ft_putstr_fd("Error: arguments\n", 2);
-		return (1);
+		if (pthread_join(g_data.philo[i].thread_id, NULL))
+		{
+			ft_putstr_fd("Error: pthread_join\n", 2);
+			return ;
+		}
+		pthread_mutex_destroy(&g_data.philo[i].right_fork);
 	}
-	if (init() || create_thread())
-		return (1);
-	exit_free();
-	return (0);
+	free(g_data.philo);
+	pthread_mutex_destroy(&g_data.display_lock);
+	pthread_mutex_destroy(&g_data.increment_lock);
 }
-
-// remember to free + destroy locks + join threads
